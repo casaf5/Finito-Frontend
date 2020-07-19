3fc12e1012e1407ea3e01961302d4c9002b388bb
Tom <tomer.tsuker@gmail.com>
2020-07-19 15:30:11 +0900

    fuck json

--- src/views/board-details.vue
+++ src/views/board-details.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div class="board-details" v-if="board">
-      <task-details v-if="taskToEdit" :taskToEdit="taskToEdit" @closeModal="closeTaskModal"/>
+      <task-details
+        v-if="taskToEdit"
+        :taskToEdit="taskToEdit"
+        @closeModal="closeTaskModal"
+      />
       <Container
         @drop="onDrop"
         drag-handle-selector=".task-group-title"
@@ -9,16 +13,20 @@
         orientation="horizontal"
       >
         <Draggable v-for="taskGroup in board.taskGroups" :key="taskGroup.id">
-          <task-group :taskGroup="taskGroup" @taskDrop="onTaskDrop" @taskClicked="openTaskModal" />
+          <task-group
+            :taskGroup="taskGroup"
+            @taskDrop="onTaskDrop"
+            @taskClicked="openTaskModal"
+          />
         </Draggable>
       </Container>
     </div>
   </div>
 </template>
- 
+
 <script>
 import { Container, Draggable } from "vue-smooth-dnd";
-import {applyDrag} from '../utils/utils.js';
+import { applyDrag } from "../utils/utils.js";
 import taskDetails from "@/components/task-details.cmp.vue";
 import taskGroup from "../components/task-group.cmp.vue";
 
@@ -31,18 +39,17 @@ export default {
   },
   data() {
     return {
-      taskToEdit:null,
+      taskToEdit: null,
       upperDropPlaceholderOptions: {
         className: "taskGroup-drop-preview",
         animationDuration: "150",
-        showOnTop: true
-      }
+        showOnTop: true,
+      },
     };
   },
   async created() {
     let id = "1E3E-1735BF480CA-26A5"; // IN REAL APP WILL COME FROM PARAMS
     await this.$store.dispatch({ type: "getBoardById", id });
-    
   },
   methods: {
     onDrop(dropResult) {
@@ -53,48 +60,39 @@ export default {
       if (dropResult.removedIndex !== null || dropResult.addedIndex !== null) {
         const board = Object.assign({}, this.board);
         const taskGroup = board.taskGroups.filter(
-          taskG => taskG.id === taskGroupId
+          (taskG) => taskG.id === taskGroupId
         )[0];
         const taskGroupIndex = board.taskGroups.indexOf(taskGroup);
         const newTaskGroup = Object.assign({}, taskGroup);
         newTaskGroup.tasks = applyDrag(newTaskGroup.tasks, dropResult);
 
-        // Because Iv'e added ParentlistId to each task to be able to find in what list it's located
-        // I had to make sure that the parentListid will change for each task that's why I'm looping
-        //over all tasks and chagning their parent
-        newTaskGroup.tasks.forEach(task => {
+        newTaskGroup.tasks.forEach((task) => {
           task.parentListId = newTaskGroup.id;
         });
-
         board.taskGroups.splice(taskGroupIndex, 1, newTaskGroup);
         await this.$store.dispatch({ type: "saveBoard", board });
-
-        /*I dont think we need this line. I want to get the board from the state
-        so the app will update without having to refresh the page */
-        // this.board = board;
       }
     },
-    openTaskModal(task){
-      this.taskToEdit=task;
+    openTaskModal(task) {
+      this.taskToEdit = task;
     },
-    closeTaskModal(){
-    this.taskToEdit=null;
-    }
+    closeTaskModal() {
+      this.taskToEdit = null;
+    },
   },
- 
+
   components: {
     Container,
     Draggable,
     taskGroup,
-    taskDetails
+    taskDetails,
   },
   computed: {
     board() {
       return this.$store.getters.board;
-    }
-  }
+    },
+  },
 };
 </script>
 
-<style scoped >
-</style>
+<style scoped></style>
