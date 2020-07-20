8e043b4041d5c9aef00ea0a587dc60c241b53b16
Tom <tomer.tsuker@gmail.com>
2020-07-20 18:37:10 +0900

    color labels

---
+++ src/components/From Elements/form-select.cmp.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="form-input-container">
+    <label v-if="showLabel">{{ labelText }}</label>
+    <select>
+      <option value=""></option>
+      <option value=""></option>
+    </select>
+    <input
+      class="form-input"
+      :type="type"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+      :placeholder="placeholder"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+    };
+  },
+  props: {
+    showLabel: {
+      type: Boolean,
+    },
+    labelText: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    placeholder: {
+      type: String,
+    },
+    value: {
+      type: String,
+    },
+    options: {
+      type: Array,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.form-input-container {
+  label {
+    text-transform: capitalize;
+    margin-top: 10px;
+    font-size: 14px;
+  }
+  .form-input {
+    margin-bottom: 5px;
+    margin-top: 5px;
+    width: 100%;
+    padding: 5px;
+    outline: none;
+    border: 1px solid #3498db;
+  }
+}
+</style>
