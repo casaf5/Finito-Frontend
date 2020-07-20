138604cd30457564b1664fe21c0d9458232a4197
Asaf Cohen <casaf5@gmail.com>
2020-07-20 18:34:15 +0300

    fixed problems with calender

--- src/views/comp-to-edit.vue
+++ src/views/comp-to-edit.vue
@@ -12,7 +12,6 @@
 <script>
 import TaskLabel from "../components/task label/taks-label.cmp";
 import CheckList from "../components/task-checklist.cmp.vue";
-import Calender from "../components/task-calander.cmp";
 import Members from "../components/task-members.cmp";
 import Select from "../components/From Elements/form-select.cmp";
 import taskAttach from "../components/task-attachment.cmp"
@@ -21,7 +20,6 @@ export default {
   components: {
     TaskLabel,
     CheckList,
-    Calender,
     Members,
     taskAttach,
   },
