27e09a089f4bbdcd36eff5c64abe20a7f0a9e09a
Asaf Cohen <casaf5@gmail.com>
2020-07-20 22:52:03 +0300

    traying to fix

--- src/views/comp-to-edit.vue
+++ src/views/comp-to-edit.vue
@@ -11,14 +11,12 @@
 <script>
 import TaskLabel from "../components/task label/taks-label.cmp";
 import CheckList from "../components/task-checklist.cmp.vue";
-import Calender from "../components/task-calander.cmp";
 import Members from "../components/task-members.cmp";
 import Select from "../components/From Elements/form-select.cmp";
 export default {
   components: {
     TaskLabel,
     CheckList,
-    Calender,
     Members,
   },
 };
