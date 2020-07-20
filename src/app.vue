fdafa8c6e7230c227d23b66f8498f66e4e0774ab
Asaf Cohen <casaf5@gmail.com>
2020-07-20 12:38:48 +0300

    minor changes

--- src/app.vue
+++ src/app.vue
@@ -1,25 +1,14 @@
 <template>
-  <div
-    id="app"
-    class="main-layout"
-  >
-    <app-header />
-    <router-view />
+  <div id="app" class="main-layout">
+      <app-header />
+      <router-view />
   </div>
 </template>
-
 
 <script>
 import appHeader from "./components/app-header.cmp.vue";
 export default {
   name: "main-app",
-  data(){
-    return {
-      isDown:false,
-      startX:null,
-      scrollLeft:null
-    }
-  },
   components: {
     appHeader
   },
