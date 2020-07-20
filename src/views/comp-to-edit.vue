6fad2df3e44c980d8a7cc7f1d24dae90be09c101
Tom <tomer.tsuker@gmail.com>
2020-07-21 08:28:20 +0900

    made a few changes to the duedata comp

--- src/views/comp-to-edit.vue
+++ src/views/comp-to-edit.vue
@@ -1,7 +1,27 @@
 <template>
   <div style="position:relative">
     <TaskLabel />
-    <formSelect :showLabel="true" labelText="board" />
+
+    <!-- <transition-group name="list-complete" tag="p">
+      <span
+        class="list-complete-item"
+        :key="index"
+        v-for="(date,index) in sortedDates"
+      >{{date.title}}</span>
+    </transition-group>-->
+    <transition-group name="flip-list" tag="ul">
+      <li :key="index" v-for="(date,index) in sortedDates">{{ date.title }}</li>
+    </transition-group>
+
+    <transition-group name="list-complete" tag="il">
+      <span
+        class="list-complete-item"
+        :key="index"
+        v-for="(date,index) in sortedDates"
+      >{{date.title}}</span>
+    </transition-group>
+
+    <button @click="sort">Sort</button>
     <!-- <CheckList /> -->
     <!-- <Calender /> -->
     <!-- <Members /> -->
@@ -15,14 +35,61 @@ import Calender from "../components/task-calander.cmp";
 import Members from "../components/task-members.cmp";
 import formSelect from "../components/From Elements/form-select.cmp";
 export default {
+  data() {
+    return {
+      dateObjects: [
+        {
+          title: "one",
+          createdAt: new Date("07-01-2020")
+        },
+        {
+          title: "most receent",
+          createdAt: new Date("07-20-2020")
+        },
+        {
+          title: "oldest",
+          createdAt: new Date("06-20-2020")
+        }
+      ]
+    };
+  },
+  methods: {
+    sort() {
+      this.dateObjects = this.dateObjects.sort(
+        (dateA, dateB) => dateB.createdAt - dateA.createdAt
+      );
+    }
+  },
+  computed: {
+    sortedDates() {
+      return this.dateObjects;
+    }
+  },
   components: {
     TaskLabel,
     CheckList,
     Calender,
     Members,
-    formSelect,
-  },
+    formSelect
+  }
 };
 </script>
 
-<style></style>
+<style>
+.list-complete-item {
+  transition: all 1s;
+  margin-right: 10px;
+  display: block;
+}
+.list-complete-enter, .list-complete-leave-to
+/* .list-complete-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translatex(30px);
+}
+.list-complete-leave-active {
+  position: absolute;
+}
+.flip-list-move {
+  transition: transform 1s;
+}
+</style>
