b371729e0df2880ceafabf2af25b7f04f88553aa
AyalMagid <ayal.magid@gmail.com>
2020-07-23 21:00:17 +0300

    update

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -2,10 +2,15 @@
   <section class="dashboard" v-if="board">
     <div class="charts-container flex col">
       <div class="linechart-container chart">
+        <div class="linechart-header flex space-between">
     <!--  make it tasks completion over time - how many completed each month-->
-       <h2>Board Progress </h2>
+       <h2>Board Progress </h2> 
+       <el-select  @input="updateLineChart" v-model="monthToStartFrom" placeholder="Show From...">
+            <el-option v-for="(month, idx) in selectOptions" :key="idx" :label="month.label" :value="month.value"></el-option>
+      </el-select>
+      </div>
        <!--  Tasks Completion Per Month since created by PREC%-->
-        <line-chart :labels="monthsToShow" :data="completedTasksPerMonth" :options="options"/>
+        <line-chart :chart-data="datacollection" :options="options"/>
       </div>
     <div class="doughnut-barchart-wraper flex">
      <div class="doughnut-container chart">
@@ -15,11 +20,11 @@
       <div class="barchart-container chart">
     <!--  make it complete and incomlete tasks per member -->
        <h2>Members Progress</h2>
-        <bar-chart :labels="boardMembersNames" :data="numOfTasksPerMember" :options="options"/>
+        <bar-chart :labels="boardMembersNames" :data="tasksNumPerMember" :options="options"/>
       </div>
      </div>
    </div>
-    {{completedTasksPerMonth}}
+    {{completedMembersTasks}}
   </section>
 </template>
 
@@ -34,6 +39,9 @@ export default {
   name: "dashboard",
     data () {
       return {
+          datacollection: {},
+          months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+          monthToStartFrom: '',
           options: {
           responsive: true,
           maintainAspectRatio: false,
@@ -44,29 +52,78 @@ export default {
                 }
             }]
           }
-        }
-      }
+        },
+        selectOptions: [{
+          value: 1,
+          label: 'Jan'
+        }, {
+          value: 2,
+          label: 'Feb'
+        }, {
+          value: 3,
+          label: 'Mar'
+        }, {
+          value: 4,
+          label: 'Apr'
+        }, {
+          value: 5,
+          label: 'May'
+        }, {
+          value: 6,
+          label: 'Jun'
+        }, {
+          value: 7,
+          label: 'Jul'
+        }, {
+          value: 8,
+          label: 'Aug'
+        }, {
+          value: 9,
+          label: 'Sep'
+        }, {
+          value: 10,
+          label: 'Oct'
+        },{
+          value: 11,
+          label: 'Nov'
+        },{
+          value: 12,
+          label: 'Dec'
+        }]
+    }
     },
     computed: {
     monthsToShow(){
-      // make if full year since board started - always 12 months but should start from the right one
-      return ['jan', 'feb', 'march', 'april', 'may']
+      const monthsForward = []
+      // make if full year since board started - always 12 months a head - add a change which accept any time to start from
+      if (!this.monthToStartFrom) {this.monthToStartFrom = new Date(this.board.createdAt).getMonth()}
+      var currMonth = this.monthToStartFrom-1
+      for (var i=0; i<12; i++) {
+        monthsForward.push(this.months[currMonth])
+        currMonth++
+        if (currMonth+1===this.monthToStartFrom) break
+        if (currMonth===12) {
+          currMonth = 0
+          }
+        }
+      return monthsForward
     },  
     board() {
       return this.$store.getters.board;
     },
-    // allTasks(){
-    //   return this.board.taskGroups.reduce((acc, tg, idx) => {
-    //     acc = acc.concat(tg.tasks)
-    //     return acc
-    //   },[])
-    // },
+    allTasks(){
+      return this.board.taskGroups.reduce((acc, tg) => {
+        acc = acc.concat(tg.tasks)
+        return acc
+      },[])
+    },
     // goes threw the relevant months in the arr, and replace them with the number of tasks was completed during that month
     completedTasksPerMonth (){
       return this.monthsToShow.map (month => {
         return this.board.activities.reduce((acc,activity) => {
-             // make sure the is a field when the activity was created and compare month "January" for example to the date inside
-          if ((activity.action === "COMPLETED_TASK") && (month === activity.ceatedAt)) acc++         
+          const creationMonth = new Date(activity.createdAt).getMonth()
+          const monthByWord = this.months[creationMonth-1]
+          if ((activity.action === "COMPLETED_TASK") && (month === monthByWord)) acc++         
           return acc
         }, 0)
       })
@@ -82,7 +139,7 @@ export default {
         return this.board.members.map(m => m.name)
     },
        // change to userName (with mongo)
-    numOfTasksPerMember(){
+    tasksNumPerMember(){
         return this.boardMembersNames.map(memberName => {
             // num ot tasks in total
             return this.board.taskGroups.reduce((higherAcc, tg) => {
@@ -98,8 +155,42 @@ export default {
             },0)
         })
     },
-
-  },
+    // check if works
+    completedMembersTasks(){
+        return this.boardMembersNames.map(memberName => {
+            return this.allTasks.reduce((acc, task) => {
+              task.members.forEach (m => {
+               if ((task.isComplete)&& (m.name === memberName))
+                  {acc ++}
+              })
+            return acc
+            },0)
+         })
+      },
+    },
+      
+    inCompletedMembersTasks(){
+      return this.tasksNumPerMember.map((memberTasksNum,idx) => {
+        memberTasksNum - (this.completedMembersTasks[idx])
+      }) 
+    },
+    mounted(){
+      this.updateLineChart()
+    },
+    methods: {
+      updateLineChart () {
+        this.datacollection = {
+          labels: this.monthsToShow,
+            datasets: [
+                        {
+                label: 'Members Progress Over a Full Year',
+                backgroundColor: '#f87979',
+                data: this.completedTasksPerMonth
+                 }
+               ]
+        }
+      },
+    },
   components:{
       doughnutChart,
       barChart,
